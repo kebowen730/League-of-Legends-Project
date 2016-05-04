f0c350ad4091708316784af2f51b45c5150654e7
TheRusBus <rusty.gates@utexas.edu>
2016-05-04 17:30:54 -0500

    Update PopStatDB.py

--- PopStatDB.py
+++ PopStatDB.py
@@ -1,14 +1,35 @@
 import LeagueScraper as ls
-import lolquery as lq
+import pymysql
+
+conn = pymysql.connect(host = '127.0.0.1', user = 'lrngsql', passwd = 'rdg935', db = 'mysql')
+cur = conn.cursor()
+cur.execute('USE League')
+
+def insertPatch(patch_id, patch_date):
+  cur.execute('SELECT * FROM Patch WHERE patch_id = %s AND patch_date = %s',(patch_id, patch_date))
+  if (cur.rowcount == 0):
+    cur.execute('INSERT INTO Patch (patch_id, patch_date) VALUES (%s, %s)', (patch_id, patch_date))
+    conn.commit()
+
+def insertStatistic(stat_id, patch_id, stat, character_name, previous_stat):
+  cur.execute('SELECT * FROM Statistic WHERE stat_id = %s AND patch_id = %s AND stat = %s AND character_name = %s AND previous_stat = %s',(stat_id, patch_id, stat, character_name, previous_stat))
+  if (cur.rowcount == 0):
+    cur.execute('INSERT INTO Statistic (stat_id, patch_id, stat, character_name, previous_stat) VALUES (%s, %s, %s, %s, %s)', (stat_id, patch_id, stat, character_name, previous_stat))
+    conn.commit()
+
+
 def populate():
     patches=[]
-    for i in ls.scrape():
-        data=i.getData()
+    stats=ls.scrape()
+    for i in stats:
+        data=i
         if data['patch_id'] not in patches:
-            patches.append([data['patch_id'],data['patch_date']])
-        lq.insertStatistic(data['stat_id'],data['patch_id'],data['stat'],data['character_name'],'0')
-    for j in patches:
-        lq.insertPatch(j[0],j[1])
+            patches.append([data['patch_id'],data['patch_date']])       
+   # for j in patches:
+    #    insertPatch(j[0],j[1])
+    for k in stats:
+        print(k)
+        insertStatistic(k['stat_id'],k['patch_id'],k['stat'],k['champ_id'],k['previous'])
 def main():
     populate()
 main()
